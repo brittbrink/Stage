8f39d3a6a558aee18e2a633e85bb9e12b245a08d
Britt Brink <485247@student.fontys.nl>
2024-01-22 15:05:30 +0100

    geprobeerd met link, maar niet mooi

--- navbar.php
+++ navbar.php
@@ -2,6 +2,7 @@
 <head>
 
     <!-- Bootstrap CSS -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/> 
     <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
@@ -12,8 +13,43 @@
 </head>
     
 <body>
+
+    <div class="row">
+        <div class="list-group" id="list-tab" role="tablist">
+            <ul class="list-group list-group-horizontal">
+                <a class="list-group-item list-group-item-flex-fill active bg-success mx-3" id="list-home-list" href="home.php" data-bs-toggle="list" role="tab" aria-controls="list-home">
+                    Home
+                </a>
+                <a class="list-group-item list-group-item-flex-fill active bg-success mx-3" id="list-profile-list" data-bs-toggle="list" href="profiel.php" role="tab" aria-controls="list-profile">
+                    Profile
+                </a>
+            </ul>
+        </div>
+    </div>
+
     
-    <nav class="navbar navbar-light bg-light">
+    <!-- <div class="row">
+        <div class="list-group" id="list-tab" role="tablist">
+            <ul class="list-group list-group-horizontal">
+                <a class="list-group-item list-group-item-flex-fill bg-success mx-3" id="list-home-list" data-bs-toggle="list" href="#list-home" role="tab" aria-controls="list-home">
+                    Home
+                </a>
+                <a class="list-group-item list-group-item-flex-fill bg-success mx-3" id="list-profile-list" data-bs-toggle="list" href="#list-profile" role="tab" aria-controls="list-profile">
+                    Profile
+                </a>
+                <a class="list-group-item list-group-item-flex-fill bg-success mx-3" id="list-messages-list" data-bs-toggle="list" href="#list-messages" role="tab" aria-controls="list-messages">
+                    Messages
+                </a>
+                <a class="list-group-item list-group-item-flex-fill bg-success mx-3" id="list-settings-list" data-bs-toggle="list" href="#list-settings" role="tab" aria-controls="list-settings">
+                    Settings
+                </a>
+
+            </ul>
+        </div>
+    </div> -->
+
+
+    <!-- <nav class="navbar navbar-light bg-light">
         <a class="navbar-brand" href="#">
             <img src="./Afbeeldingen/SCIOS-logo.png" width="40" height="40" alt="">
         </a>
@@ -26,7 +62,7 @@
             </li>
         </ul>
     
-    </nav> 
+    </nav>  -->
 
     
 </body>
